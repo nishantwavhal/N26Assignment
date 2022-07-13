4eb7ac1f524aefb9d81dc2c1d1358ec84774692e
nishantwavhal <wavhalnishant@gmail.com>
2022-07-14 00:19:55 +0200

    Add Changes to main

--- force-app/main/default/lwc/product_InfoClient/product_InfoClient.js
+++ force-app/main/default/lwc/product_InfoClient/product_InfoClient.js
@@ -1,50 +1,38 @@
+/*
+Author: Nishant Mohan Wavhal
+Created Date: 7/11/2022
+Lightning Web Component for Product Info
+*/
 import { LightningElement,wire,api,track } from 'lwc';
 import getCostPerService from '@salesforce/apex/Product_Info_Controller.cost_per_Services';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
+export default class Product_InfoClient extends LightningElement{
+@api recordId;
+@track mdtwrap=[];
+@track error;
 
-
-export default class Product_InfoClient extends LightningElement {
-    @api recordId;
-    @track mdtwrap=[];
-    @track error;
- 
-   
-
-
-    connectedCallback() {
-
-        console.log("recordId", this.recordId);
-        this.handleLoad();
+    connectedCallback()
+    {
+      this.handleLoad();
     }
- 
 
-    handleLoad() {
-    getCostPerService({caseId:this.recordId}).then(result => {
-        try{
-             this.mdtwrap=result;
-             //console.log("Hello world ", JSON.stringify(this.mdtwrap));
-        }catch( e)
-        {     
-       console.error('Error',e);
-        }
-    
-  
-    })
-    .catch(error => {
-        this.error = error;
-        console.log('Error',this.error);
-        this.dispatchEvent(
-            new ShowToastEvent({
+    handleLoad() 
+    {
+        getCostPerService({caseId:this.recordId})
+            .then(result => {
+                this.mdtwrap=result;
+            })
+            .catch(error => {
+                this.error = error;
+                console.log('Error',this.error);
+                this.dispatchEvent(
+                new ShowToastEvent({
                 title: "Error",
                 message:this.error.body.message,
                 variant: "Error",
             }),
-        );
-        
-    });
-}
-
-
-   
+          );    
+        });
+    }
 }
